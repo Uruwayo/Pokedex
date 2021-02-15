432936d47593c7e179eb8326bca7dae8d23a87af
Manuel Romero <manolorh55@gmail.com>
2021-02-15 13:48:51 +0100

    F001 Coroutines

--- app/src/main/java/com/example/pokedex/ui/fragment/list/ListViewModel.kt
+++ app/src/main/java/com/example/pokedex/ui/fragment/list/ListViewModel.kt
@@ -1,6 +1,15 @@
 package com.example.pokedex.ui.fragment.list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ListViewModel : ViewModel() {
+
+    fun requestInfo(){
+        viewModelScope.launch(Dispatchers.IO) {
+
+        }
+    }
 }
