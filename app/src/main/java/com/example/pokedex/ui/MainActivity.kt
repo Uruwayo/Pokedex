9f6f9cc77286ebe6e6e3fca16579afc9d460f4e7
Manuel Romero <manolorh55@gmail.com>
2021-02-15 13:30:23 +0100

    F001 DataBinding

--- app/src/main/java/com/example/pokedex/ui/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/ui/MainActivity.kt
@@ -3,10 +3,17 @@ package com.example.pokedex.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pokedex.R
+import com.example.pokedex.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/pokedex/ui/fragment/detail/DetailFragment.kt
+++ app/src/main/java/com/example/pokedex/ui/fragment/detail/DetailFragment.kt
@@ -6,13 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.pokedex.R
+import com.example.pokedex.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+    lateinit var binding: FragmentDetailBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentDetailBinding.inflate(inflater, container, false)
+
+
+        return binding.root
     }
 
 }
